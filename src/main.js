5369843e9d5093d0d76fb60b91afc72c990f7a8f
Ali Parvizi <snparvizi75@gmail.com>
2020-07-19 12:57:49 +0430

    router history mode after tring n million different option :/ (I hate frontend, I just fukcing hate it)

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import router from './router'
-
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 Vue.config.productionTip = false
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  routes: []
+})
 
 new Vue({
   vuetify,
-  router,
+  router ,
   render: h => h(App)
 }).$mount('#app')
