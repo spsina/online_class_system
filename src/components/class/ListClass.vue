07d8f029824e030db67e7837b5bde70e8c1e579f
Ali Parvizi <snparvizi75@gmail.com>
2020-07-20 15:38:10 +0430

    class list table design

--- src/components/class/ListClass.vue
+++ src/components/class/ListClass.vue
@@ -1,14 +1,7 @@
 <template>
     <div >
         <v-row>
-            <v-col 
-                cols="6"
-                md="3"
-                align-self="center"
-                style="text-align: center"
-                >
-            <h3 class="mx-auto heading">لیست کلاس ها</h3>
-            </v-col>
+            
             <v-spacer></v-spacer>
             <v-col 
                 cols="6"
@@ -21,8 +14,57 @@
                 </v-btn>
             </v-col>
         </v-row>
-        <v-row v-if="!classes.length">
-            <p class="mx-auto my-10 grey--text">هنوز هیچ کلاسی اضاف نشده :(</p>
+        <v-row>
+            <v-col cols="12">
+                <v-card>
+                    <v-card-title>
+                        کلاس ها
+                        <v-spacer></v-spacer>
+                        <v-text-field
+                            v-model="search"
+                            append-icon="mdi-magnify"
+                            label="جست و جو"
+                            single-line
+                            hide-details
+                        ></v-text-field>
+                    </v-card-title>
+                    
+                    <v-data-table
+                        :headers="headers"
+                        :items="classes"
+                        :search="search"
+                        :loading="loading"
+                        :footer-props="{
+                            'pageText': 'sadf',
+                            showFirstLastPage: true,
+                            'items-per-page-text':'کلاس در صفحه',
+                        }"
+                        no-data-text="هنوز کلاسی اضافه نشده :("
+                    >
+
+                    <template v-slot:footer.page-text="items"> 
+                        {{ items.pageStart }} - {{ items.pageStop }} از {{ items.itemsLength }} 
+                    </template>
+
+                    <template v-slot:item.actions="{ item }">
+                        <v-icon
+                            small
+                            class="mr-2"
+                            @click="editItem(item)"
+                        >
+                            mdi-pencil
+                        </v-icon>
+                        <v-icon
+                            small
+                            @click="deleteItem(item)"
+                        >
+                            mdi-delete
+                        </v-icon>
+                    </template>
+                    </v-data-table>
+
+                </v-card>
+            </v-col>
         </v-row>
     </div>
 </template>
@@ -31,10 +73,69 @@
 export default {
     data() {
         return {
-            classes: []
+            rppt: "ردیف در صفحه",
+            classes: [
+                {
+                    class_name: 'برنامه نویسی وب',
+                    teacher: 'علی پرویزی',
+                    students_count: 15
+                },
+                {
+                    class_name: 'برنامه نویسی وب',
+                    teacher: 'علی پرویزی',
+                    students_count: 15
+                },
+                {
+                    class_name: 'برنامه نویسی وب',
+                    teacher: 'علی پرویزی',
+                    students_count: 15
+                },
+                {
+                    class_name: 'برنامه نویسی وب',
+                    teacher: 'علی پرویزی',
+                    students_count: 15
+                },
+                {
+                    class_name: 'برنامه نویسی وب',
+                    teacher: 'علی پرویزی',
+                    students_count: 15
+                },
+                {
+                    class_name: 'برنامه نویسی وب',
+                    teacher: 'علی پرویزی',
+                    students_count: 15
+                }
+            ],
+            headers: [
+                {
+                    text: 'نام کلاس',
+                    align: 'start',
+                    sortable: true,
+                    value: 'class_name',
+                },
+                 {
+                    text: 'مدرس',
+                    align: 'start',
+                    sortable: true,
+                    value: 'teacher',
+                },
+                {
+                    text: 'تعداد دانشجو',
+                    align: 'start',
+                    sortable: true,
+                    value: 'students_count',
+                },
+                {
+                    text: 'عملیات',
+                    align: 'start',
+                    value: "actions",
+                    sortable: false,
+                },
+            ],
+            search: '',
+            loading: false
         }
     },
-
     mounted() {
     }
 }
